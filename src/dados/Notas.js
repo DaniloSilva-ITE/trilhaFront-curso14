c3c48dcee270ce439565e64ffc2bb763fedf84c3
Danilo Silva <danilo.silva@itenabler.com.br>
2021-05-18 15:43:33 -0300

    Cap 5 e Conclusão

--- src/dados/Notas.js
+++ src/dados/Notas.js
@@ -3,24 +3,30 @@ export default class ArrayDeNotas{
     this.notas = [];
     this._inscritos = [];
   }
-
-  adicionarNota(titulo, texto, categoria){
-    const novaNota = new Nota(titulo, texto, categoria);
-    this.notas.push(novaNota);
-  }
-
-  apagarNota(indice){
-    this.notas.splice(indice,1);
-  }
-
+  
   inscrever(func){
     this._inscritos.push(func);
+  }
+
+  desinscrever(func){
+    this._inscritos = this._inscritos.filter(f => f !== func);
   }
 
   notificar(func){
     this._inscritos.forEach(func => {
       func(this.notas);
     });
+  }
+
+  adicionarNota(titulo, texto, categoria){
+    const novaNota = new Nota(titulo, texto, categoria);
+    this.notas.push(novaNota);
+    this.notificar();
+  }
+
+  apagarNota(indice){
+    this.notas.splice(indice,1);
+    this.notificar();
   }
 }
 
